23a54c3be7771669a50a7dbac86f167daed59e66
oliveirag <113860797+oliveirag@users.noreply.github.com>
2025-08-12 13:40:31 -0400

    Initial Commit

---
+++ TabTogetherTests/TabTogetherTests.swift
@@ -0,0 +1,16 @@
+//
+//  TabTogetherTests.swift
+//  TabTogetherTests
+//
+//  Created by Guilherme Oliveira on 8/12/25.
+//
+
+import Testing
+
+struct TabTogetherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
